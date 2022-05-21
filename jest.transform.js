51d7782e932e6f3b83610b6821d6f23bc3196503
eugenesvk <eugenesvk@users.noreply.github.com>
2022-05-21 22:47:42 +0700

    test: jest: move targets out of presets
    Targets are only 'Allowed in Babel's programmatic options, or in config files'

--- jest.transform.js
+++ jest.transform.js
@@ -1,5 +1,5 @@
 const babelOptions = {
-  presets: [['env', { targets: { node:'14' } }], 'react'],
-  plugins: [['@babel/plugin-transform-react-jsx', { pragma:'h' } ]  ]
+  presets: [['@babel/preset-env', {targets:{node:'14.19'}}], '@babel/preset-react'],
+  plugins: [['@babel/plugin-transform-react-jsx', {pragma:'h'}]]
 };
 module.exports = require('babel-jest').createTransformer(babelOptions);
