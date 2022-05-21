0598d74ec190ba645ab868f0d36c0acddefed93e
eugenesvk <eugenesvk@users.noreply.github.com>
2022-05-21 18:19:00 +0700

    reformat jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,30 +1,26 @@
 module.exports = {
   moduleNameMapper: {
-    '@/(.*)$': '<rootDir>/src/$1',
-    '^src/(.*)$': '<rootDir>/src/$1',
-    '^suggestion_utils/(.*)$': '<rootDir>/src/suggestion_utils/$1',
-    '^suggestion_engine/(.*)$': '<rootDir>/src/suggestion_engine/$1',
-    '^lib/(.*)$': '<rootDir>/src/lib/$1',
-    '^msg/(.*)$': '<rootDir>/src/msg/$1',
-    '^.*\\.(css|less|sass|scss)$': '<rootDir>/test/__mocks__/styleMock.scss',
-    '^react-dom/server$':
-      '<rootDir>/node_modules/preact-render-to-string/dist/index.js',
-    '^react-addons-test-utils$':
-      '<rootDir>/node_modules/preact-test-utils/lib/index.js',
-    '^react$': '<rootDir>/node_modules/preact-compat/lib/index.js',
-    '^react-dom$': '<rootDir>/node_modules/preact-compat/lib/index.js'
+    '@/(.*)$':                    '<rootDir>/src/$1',
+    '^src/(.*)$':                 '<rootDir>/src/$1',
+    '^suggestion_utils/(.*)$':    '<rootDir>/src/suggestion_utils/$1',
+    '^suggestion_engine/(.*)$':   '<rootDir>/src/suggestion_engine/$1',
+    '^lib/(.*)$':                 '<rootDir>/src/lib/$1',
+    '^msg/(.*)$':                 '<rootDir>/src/msg/$1',
+    '^.*\\.(css|less|sass|scss)$':'<rootDir>/test/__mocks__/styleMock.scss',
+    '^react-dom/server$':         '<rootDir>/node_modules/preact-render-to-string/dist/index.js',
+    '^react-addons-test-utils$':  '<rootDir>/node_modules/preact-test-utils/lib/index.js',
+    '^react$':                    '<rootDir>/node_modules/preact-compat/lib/index.js',
+    '^react-dom$':                '<rootDir>/node_modules/preact-compat/lib/index.js'
   },
   moduleFileExtensions: ['js', 'jsx'],
-  transform: {
-    '^.+\\.(js|jsx)?$': '<rootDir>/jest.transform.js'
-  },
+  transform: { '^.+\\.(js|jsx)?$': '<rootDir>/jest.transform.js' },
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{js,jsx}'],
   coverageThreshold: {
     global: {
-      branches: 59,
+      branches:  59,
       functions: 65,
-      lines: 62
+      lines:     62
     }
   },
   coverageReporters: ['json', 'lcov', 'html'],
--- jest.transform.js
+++ jest.transform.js
@@ -1,12 +1,5 @@
 const babelOptions = {
-  presets: [['env', { targets: { node: '8' } }], 'react'],
-  plugins: [
-    [
-      '@babel/plugin-transform-react-jsx',
-      {
-        pragma: 'h'
-      }
-    ]
-  ]
+  presets: [['env', { targets: { node:'14' } }], 'react'],
+  plugins: [['@babel/plugin-transform-react-jsx', { pragma:'h' } ]  ]
 };
 module.exports = require('babel-jest').createTransformer(babelOptions);
