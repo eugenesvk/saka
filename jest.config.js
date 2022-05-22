09d37ddeee2fd56a427bf645874331afe724459f
eugenesvk <eugenesvk@users.noreply.github.com>
2022-05-22 13:48:47 +0700

    test: jest: retain the default jsdom environment post v27

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
     '^react$':                    'preact/compat',
     '^react-dom$':                'preact/compat'
   },
+  testEnvironment: 'jsdom',
   moduleFileExtensions: ['js', 'jsx'],
   transform: { '^.+\\.(js|jsx)?$': '<rootDir>/jest.transform.js' },
   collectCoverage: true,
