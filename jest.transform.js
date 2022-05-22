3d50b15f6c4e669245441eaf79cdee1c669adeab
eugenesvk <eugenesvk@users.noreply.github.com>
2022-05-22 20:37:20 +0700

    test: fix require babel-jest error in jest v27

--- jest.transform.js
+++ jest.transform.js
@@ -2,4 +2,4 @@ const babelOptions = {
   presets: [['@babel/preset-env', {targets:{node:'14.19'}}], '@babel/preset-react'],
   plugins: [['@babel/plugin-transform-react-jsx', {pragma:'h'}]]
 };
-module.exports = require('babel-jest').createTransformer(babelOptions);
+module.exports = require('babel-jest').default.createTransformer(babelOptions);
